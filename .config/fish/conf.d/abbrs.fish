89aca60b87eb3a37d3ab85949ddf81af648032a4
valerie <204412357+x-9917638@users.noreply.github.com>
2026-01-06 18:18:42 +1100

    update abbr, new update

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -1,2 +1,4 @@
 
 abbr -a stow-dotfiles "stow -S --ignore README.md --ignore .wakatime-proj --ignore usr --ignore etc ."
+
+abbr -a update-mirrors "run0 ghostmirror -Po -mu /etc/pacman.d/mirrorlist -l /etc/pacman.d/mirrorlist -s light -S state,outofdate,morerecent,estimated,speed"
